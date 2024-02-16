7ec464118a70b09b20eb92e4f487d28a391c5b57
ahmed-niaz <niazahmed.an@gmail.com>
2024-02-16 19:00:22 +0600

    complete the discount and total price

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ for (const card of cards) {
     document.querySelector(`#totalPrice`).innerText = totalPrice.toFixed(2);
   });
 }
+
+document.querySelector(`#apply-btn`).addEventListener(`click`, function () {
+  const promoCode = document.querySelector(`#input-field`).value;
+  const code = promoCode.split(" ").join("").toUpperCase();
+
+  if (totalPrice >= 200) {
+    if (code === `SELL200`) {
+      const discountElement = document.querySelector(`#discountPrice`);
+      const discountPrice = totalPrice * 0.2;
+      discountElement.innerText = discountPrice.toFixed(2);
+      const restAmount = document.querySelector(`#total`);
+      restAmount.innerText = totalPrice - discountPrice.toFixed(2);
+      document.querySelector(`#input-field`).value = "";
+    } else {
+      alert(`Invalid promo Code`);
+    }
+  } else {
+    alert(`purchase at least 200 $`);
+  }
+});
